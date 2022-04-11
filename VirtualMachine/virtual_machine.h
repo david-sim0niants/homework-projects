efd6e032fbd95796139ead9c1772ccf9091bf3cf
David <david.simonyants@gmail.com>
2022-04-11 22:02:47 +0400

    header documented

--- VirtualMachine/virtual_machine.h
+++ VirtualMachine/virtual_machine.h
@@ -12,24 +12,6 @@
 
 
 
-enum OpCode : uint8_t
-{
-    ADD = 0,
-    SUB = 1,
-    AND = 2,
-    OR = 3,
-    NOT = 4,
-    XOR = 5,
-
-    IF_EQ = 32,
-    IF_NOT_EQ = 33,
-    IF_LESS = 34,
-    IF_LESS_OR_EQ = 35,
-    IF_GREATER = 36,
-    IF_GREATER_OR_EQ = 37
-};
-
-
 constexpr unsigned char FIRST_IMMEDIATE = 64;
 constexpr unsigned char SECOND_IMMEDIATE = 128;
 
@@ -72,18 +54,27 @@ public:
 class VirtualMachine
 {
 public:
+    // total number of registers
     static constexpr size_t NUM_REGISTERS = 16;
+    // number of general purpose registers
     static constexpr size_t NUM_GP_REGISTERS = NUM_REGISTERS - 2;
 
+    // index of input or output registers
     static constexpr size_t IO_REG_INDEX = NUM_GP_REGISTERS;
+    // index of counter register
     static constexpr size_t COUNTER_INDEX = NUM_GP_REGISTERS + 1;
 
 private:
+    // RAM basically
     std::vector<char> memory;
-    std::array<uint32_t, NUM_REGISTERS> gp_registers;
+    // general purpose registers indexed from 0 to NUM_GP_REGISTERS - 1
+    std::array<uint32_t, NUM_GP_REGISTERS> gp_registers;
+    // counter register
     uint32_t counter;
 
+    // input stream connected to input register
     std::istream *input = nullptr;
+    // output stream connected to output register
     std::ostream *output = nullptr;
 
 public:
@@ -107,14 +98,22 @@ public:
     }
 
 
+    // upload program from input stream to memory
     void upload_Program(std::istream &program);
+    // execute single instruction and increase counter by the size of instruction
     void exec();
+    // run until counter stops changing (hanging)
     void run();
 
 private:
+    // calculate source value from src register or memory
+    // read from register if src < NUM_REGISTERS, otherwise read from memory
     uint32_t get_SrcValue(uint8_t src);
+    // set destination value
+    // write to register if dst < NUM_REGISTERS, otherwise write to memory
     void set_DstValue(uint8_t dst, uint32_t value);
 
+    // ALU ops
     static uint32_t op_ADD(uint32_t src1, uint32_t src2);
     static uint32_t op_SUB(uint32_t src1, uint32_t src2);
     static uint32_t op_AND(uint32_t src1, uint32_t src2);
@@ -123,6 +122,7 @@ private:
     static uint32_t op_XOR(uint32_t src1, uint32_t src2);
     static uint32_t op_MUL(uint32_t src1, uint32_t src2);
 
+    // COND ops
     void op_IF_EQ           (uint32_t src1, uint32_t src2, uint32_t dst);
     void op_IF_NOT_EQ       (uint32_t src1, uint32_t src2, uint32_t dst);
     void op_IF_LESS         (uint32_t src1, uint32_t src2, uint32_t dst);
@@ -133,8 +133,8 @@ private:
     using ALU_op = uint32_t (*) (uint32_t, uint32_t);
     using COND_op = void (VirtualMachine::*) (uint32_t, uint32_t, uint32_t);
 
-    static ALU_op ALU_ops[7];
-    static COND_op COND_ops[6];
+    static ALU_op ALU_ops[7]; // array of ALU ops
+    static COND_op COND_ops[6]; // array of COND ops
 };
 
 
