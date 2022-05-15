39449ded0c2ab041420e289aa4cc6ff329247191
David <david.simonyants@gmail.com>
2022-05-15 14:17:59 +0400

    MBR: can now laod itself to a location 0x600

--- MBR_on_x86/boot.s
+++ MBR_on_x86/boot.s
@@ -1,27 +1,34 @@
 .code16
-
 .section .boot
-
+.extern init_RealMode
 
 _start:
+    # disable interrupts
     cli
+    # set segment registers (except cs for a strange reason) to zero
     xor %ax, %ax
     mov %ax, %ds
     mov %ax, %ss
     mov %ax, %es
     mov %ax, %bp
 
+    # copy this MBR code to a location 0x600
+    # all the addresses here will be offseted by 0x600 by linker.ld script
     mov $0x0100, %cx
     mov $0x7c00, %si
     mov $0x0600, %di
     rep movsw
 
-    jmpl $0x0600, $_continue
+    # continue in a newly copied code
+    jmp _continue
 
 _continue:
+    # enable interrupts
     sti
+    # set stack pointer
     mov $0x0600, %sp
 
+    # print "Hello world!"
     mov $msg, %si
     call print
     hlt
@@ -30,9 +37,11 @@ _continue:
 .type print, @function
 
 print:
+    # enable 'write in tty' mode in BIOS
     mov $0xE, %ah
 
 .L_print_loop:
+    # load and print string bytewise
     lodsb
     cmp $0, %al
     je .L_print_exit
